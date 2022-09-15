5c619ab94ad61ee792fd46761ee59e73f7c5c994
Beam-ki <skunn123@gmail.com>
2022-09-15 19:56:00 +0900

    심화계산기 수정 ,리스트 필터 수정

--- 14/calc.py
+++ 14/calc.py
@@ -1,7 +1,7 @@
 class Calc():
-    def set_number(number1,number2):
-        number1=number1
-        number2=number2
+    def set_number(self,number1,number2):
+        self.number1=number1
+        self.number2=number2
     def plus(self):
         return number1+number2
     def minus(self):
@@ -10,23 +10,25 @@ class Calc():
         return number1*number2
     def divide(self):
         try: 
-            number2==0
+            return number1/number2
+        except ValueError:
             print("0으로 나눌수 없습니다.")
-        except:
-            return
+        except ZeroDivisionError:
+            print("0으로 나눌수 없습니다.")
 calc = Calc()
-try:
-    number1 = int(input())
-    number2 = int(input())
+while True:
+    try:
+        # number1 = int(input())
+        # number2 = int(input())
+        number1,number2=[int(x) for x in input().split(" ")]
 
-    int(number1) and int(number2)
-except ValueError:
-        print("숫자를 입력해주세요.")
+    except ValueError:
+            print("숫자를 입력해주세요.")
 
-print(calc.plus()) # 더한 값
-print(calc.minus()) # 뺀 값
-print(calc.multiply()) # 곱한 값
-print(calc.divide()) # 나눈 값
+    print(calc.plus()) # 더한 값
+    print(calc.minus()) # 뺀 값
+    print(calc.multiply()) # 곱한 값
+    print(calc.divide()) # 나눈 값
 
 # if number2==0
     #print("0으로 나눌수 없습니다.")
